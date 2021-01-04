ad3ba38695d0502ded4804cf91c93b1f9fe71aaf
Anish Pokhrel <66049909+apokhrel7@users.noreply.github.com>
2021-01-03 22:23:57 -0700

    Update Main.java

--- src/problem2/Main.java
+++ src/problem2/Main.java
@@ -7,9 +7,12 @@ class Problem2 {    //having no public/private means class is only accessible to
 
         if (basePay < 8) {
             System.out.println("Base pay can't be less than $8.00 an hour");
-        } else if (hours > 60) {
+        } 
+        else if (hours > 60) {
             System.out.println("Employee can't work for more than 60 hours");
-        } else {
+        } 
+        
+        else {
             int overtimeHours = 0;
 
             if (hours > 40) {
@@ -24,31 +27,6 @@ class Problem2 {    //having no public/private means class is only accessible to
 
         }
     }
-
-    public void squareRoot(double number, double approximation) {
-
-        double lower_bound = 0d;
-        double upper_bound = number;
-        byte steps = 0;
-
-        double possible_root = number;
-
-        while ((Math.abs(number - possible_root * possible_root)) >= approximation) {
-            if (possible_root * possible_root > number) {
-                upper_bound = possible_root;
-            } else {
-                lower_bound = possible_root;
-            }
-            possible_root = (upper_bound + lower_bound) / 2;
-            steps++;
-        }
-        // System.out.printf() prints in with a format
-        System.out.printf("Square root of %s is about %s. ", number, possible_root); //put %n at end to create new line
-
-        String ternary = (steps == 1) ? steps + " step" : steps + " steps";
-        System.out.println("It took " + ternary + " to get the root");
-    }
-
 }
 
 
@@ -57,22 +35,10 @@ class Problem2 {    //having no public/private means class is only accessible to
 class Main {
 
     public static void main(String[] args) {
-        // To run in cmd -> C:\Users\Anish\IdeaProjects\learning_java_\src>java com.company.Problem1.problem1.Main
-        // typing "sout" is a shortcut for System.out.println();
-
+ 
         Problem2 salary = new Problem2();
         salary.calcSalary(7.85, 43);
 
-        Problem2 root_calculator = new Problem2();
-        root_calculator.squareRoot(7, 0.001);
-
-
-
-        /*
-        This is a multi-line comment
-        This is a multi-line comment
-        This is a multi-line comment
-        */
 
     }
 
